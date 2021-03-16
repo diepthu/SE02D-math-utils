d4145128f97ee1a48fc681d5e08e1f844c9d5171
diepthu <thanhtrongxyz@gmail.com>
2021-03-16 17:28:16 +0700

    1st uploadgit add *!

---
+++ src/se02d/math/utils/SE02DMathUtils.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se02d.math.utils;
+
+/**
+ *
+ * @author trong
+ */
+public class SE02DMathUtils {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("1st upload from local to GitHub!!");
+        System.out.println("Wellcom to GitHub");
+    }
+    
+}
